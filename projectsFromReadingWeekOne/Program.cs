55b613ac9c11e5b0c12dcb3b896cca0c6e252122
PensCup87 <bdt9123@gmail.com>
2017-10-01 18:58:40 -0400

    Completed work from Chapter 9 and Part of 10

--- projectsFromReadingWeekOne/Program.cs
+++ projectsFromReadingWeekOne/Program.cs
@@ -10,6 +10,76 @@ namespace projectsFromReadingWeekOne
     {
         static void Main(string[] args)
         {
+            //float totalCost = 22.54f;
+            //float tipPercent = 0.18f;
+            //float tipAmount = totalCost * tipPercent;
+            //Console.WriteLine("Tip amount is $" + tipAmount);
+
+            //float b = 1.5f;
+            //float h = 4f;
+            //float triangleArea = .5f * (b * h);
+            //Console.WriteLine("Triangle area equals " + triangleArea);
+
+            //int x = 9;
+            //int y = 99;
+            //int quotient = y / x;
+            //int remainder = y % x;
+            //Console.WriteLine(quotient);
+            //Console.WriteLine(remainder);
+
+            //int t = +3;
+            //Console.WriteLine(t);
+
+            ////Compound Assingment Operators
+            //int q = 5;
+            //q += 3;
+            //Console.WriteLine(q);
+
+            ////Calculating Cylinder Volume
+            //float pi = 3.1415926f;
+
+
+            //Console.WriteLine("Hello, time for a cylinder calculation.  Just need 2 numbers.");
+            //Console.WriteLine("Please enter a value for the cylinder height: ");
+            //int cylHeight = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Now enter a value for the cylinder radius: ");
+            //int cylRadius = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("The formula for the volume is:\nV = pi*(r^2)*h");
+
+            //Console.WriteLine("We are also going to calculate the surface area \nSA = 2pi(r)*(r+h)");
+
+            //double volume = ((pi * (cylRadius^2)) * cylHeight);
+            //double surfaceArea = ((2 * pi * cylRadius) * (cylRadius + cylHeight));
+
+            //Console.WriteLine("The cylinder volumen is " + volume + " cubic units");
+            //Console.WriteLine("The cylinder surface area is " + surfaceArea + " square units");
+
+            //Console.ReadKey();
+
+            //More Math
+
+            int e = 7;
+            int f = 2;
+            int efResult = e / f;
+            Console.WriteLine(efResult);
+
+            float g = 7.0f;
+            float h = 2.0f;
+            float final = g / h;
+            Console.WriteLine(final);
+
+            int i = 9;
+            float j = 4;
+            float determination = i / j;
+            Console.WriteLine(determination);
+
+
+
+            
+
+
         }
     }
 }
